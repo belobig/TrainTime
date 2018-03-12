7d08d0e915f5b7d43e833bb9265a71a9d9bec5bb
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-03-11 19:49:13 -0600

    Fixed auto-update of minutes away

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TrainTime
-A Train schedule site
+A Train schedule site that updates automatically
 https://belobig.github.io/TrainTime
--- assets/javascript/trainTime.js
+++ assets/javascript/trainTime.js
@@ -30,6 +30,11 @@ var tRemainder;
 var getKey;
 var key;
 var dateAdded;
+var minsID;
+var arrivalID;
+var minsIdArray = [];
+var arrivalIdArray = [];
+
 
 // Clock function to display current time
 function displayTime() {
@@ -42,7 +47,7 @@ setInterval(displayTime, 1000);
 
 $("#submitInfo").on("click", function (event) {
 	// Prevent form from reloading the page on Submit
-	event.preventDefault();
+	// event.preventDefault();
 
 	// Get the input values
 	name = $("#trainName").val().trim();
@@ -59,38 +64,68 @@ $("#submitInfo").on("click", function (event) {
 		freq: freq,
 		dateAdded: firebase.database.ServerValue.TIMESTAMP
 	});
-	
+
 
 });
 // console.log(database.ref());
 
 // Each time a child, or train, is added to the database, add it to the DOM
 database.ref().on("child_added", function (snapshot) {
-	console.log("I had a child!");
+	//console.log("I had a child!");
 	tdName = snapshot.val().name;
 	tdDest = snapshot.val().dest;
 	tdFirstTrain = snapshot.val().firstTrain;
 	tdFreq = snapshot.val().freq;
-	firstTimeConv = moment(tdFirstTrain, "hh:mm")//.subtract(1, "years");
+	firstTimeConv = moment(tdFirstTrain, "hh:mm")
 	timeDiff = moment().diff(moment(firstTimeConv), "minutes");
 	tRemainder = timeDiff % tdFreq;
 	tdMinutes = tdFreq - tRemainder;
 	tdNextArrival = moment().add(tdMinutes, "minutes").format("h:mm A");
 	key = snapshot.Ce.key;
-	console.log(snapshot.Ce.key);// This is how I figured out how to get the key
-	
+	//console.log(snapshot.Ce.key);// This is how I figured out how to get the key
+	minsID = 'mins' + key;
+	arrivalID = 'arrival' + key;
 
-	$("#trainTable").append("<tr id=" + "'"  + key + "'" + "><td>" + tdName + "</td><td>" + tdDest + "</td><td>" + tdFirstTrain + "</td><td>" + tdFreq + "</td><td>" + tdNextArrival + "</td><td>" + tdMinutes + "</td>" + "<td><input type='submit' value='Remove Train' class='remove-train btn btn-danger btn-sm'></td></tr>");
-	
+	$("#trainTable").append("<tr id=" + "'" + key + "'" + "><td>" + tdName + "</td><td>" + tdDest + "</td><td>" + tdFirstTrain + "</td><td>" + tdFreq + "</td><td class='updateableArrival' id=" + "'" + arrivalID + "'" + ">" + tdNextArrival + "</td><td class='updateableMins' id=" + "'" + minsID + "'" + ">" + tdMinutes + "</td>" + "<td><input type='submit' value='Remove Train' class='remove-train btn btn-danger btn-sm'></td></tr>");
+	// console.log(minsID + " " + arrivalID);
+	// console.log(snapshot.val());
+	// console.log($("#" + minsID + "").html());
+	minsIdArray.push($("#" + minsID + "").attr('id'));
+	arrivalIdArray.push($("#" + arrivalID + "").attr('id'));
 });
+// }
 
-// TODO: Get the Next Arrival and Minutes Away fields to update automatically
+// DONE: Get the Next Arrival and Minutes Away fields to update automatically
+function updateMinutes() {
+	database.ref().once("value", function (snapshot) {
+		snapshot.forEach(function (childSnapshot) {
+			var updtFirstTrain = childSnapshot.val().firstTrain;
+			var updtFreq = childSnapshot.val().freq;
+			var updtfirstTimeConv = moment(updtFirstTrain, "hh:mm")
+			var updttimeDiff = moment().diff(moment(updtfirstTimeConv), "minutes");
+			var updttRemainder = updttimeDiff % updtFreq;
+			var updtMinutes = updtFreq - updttRemainder;
+			var updtNextArrival = moment().add(updtMinutes, "minutes").format("h:mm A");
+			// console.log(childSnapshot.val().firstTrain);
+			var updtKey = childSnapshot.Ce.key;
+			var updtMinsID = 'mins' + updtKey;
+			var updtArrivalID = 'arrival' + updtKey;
 
-// setInterval(updateInfo, 1000);
+			// console.log(parseInt($("#" + updtMinsID + "").html()));
+			$("#" + updtMinsID + "").html(updtMinutes);
+			$("#" + updtArrivalID + "").html(updtNextArrival);
+		});
+
+	});
+
+}
+setInterval(updateMinutes, 1000);
+
 
 // Remove the row of train information when the Remove Train button is clicked
-$("body").on("click", ".remove-train", function(){
-	$(this).closest ('tr').remove();
+$("body").on("click", ".remove-train", function () {
+	$(this).closest('tr').remove();
 	getKey = $(this).parent().parent().attr('id');
 	database.ref().child(getKey).remove();
 });
+
